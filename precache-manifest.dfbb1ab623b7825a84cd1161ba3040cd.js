21d673d3c7905ccc0b0c0d1edc191f45284e2c67
alejandro <alexanderbasoc@gmail.com>
2020-05-23 15:12:01 -0700

    Updates

---
+++ precache-manifest.dfbb1ab623b7825a84cd1161ba3040cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f67b22986c5ec645e8fd5debc3d18a",
+    "url": "/react-material-mini-landing/index.html"
+  },
+  {
+    "revision": "3d001950b0d8a949894e",
+    "url": "/react-material-mini-landing/static/css/main.0284c47e.chunk.css"
+  },
+  {
+    "revision": "6710a437ed638d329e28",
+    "url": "/react-material-mini-landing/static/js/2.9daf8d02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-material-mini-landing/static/js/2.9daf8d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d001950b0d8a949894e",
+    "url": "/react-material-mini-landing/static/js/main.cf1b8e5a.chunk.js"
+  },
+  {
+    "revision": "099e03fe64dcf24a4079",
+    "url": "/react-material-mini-landing/static/js/runtime-main.bc6b013f.js"
+  },
+  {
+    "revision": "a161c642bcbabb2a3a94fb008a01cf30",
+    "url": "/react-material-mini-landing/static/media/hero.a161c642.jpg"
+  }
+]);
